a4989a48e3ade19083f13555cff68d911597218d
Paul Chung <paul.chung@innocube.net>
2015-02-26 08:40:21 +0900

    Added a Feature to Add New Task
    - Add New Task
    - Sort According to Created Time

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,24 @@ if (Meteor.isClient) {
 
     Template.body.helpers({
         tasks: function(){
-            return Tasks.find({});
+            return Tasks.find({}, {sort: {createdAt: -1} });
+        }
+    });
+
+    Template.body.events({
+        'submit .new-task': function(event){
+            var text = event.target.text.value;
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
+
+            // Clear form
+            event.target.text.value = "";
+
+            // Prevent default form submit
+            return false;
         }
     });
 }
